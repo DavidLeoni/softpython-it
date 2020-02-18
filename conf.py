9b3f13e3d0179d956c65a396a89f283664958316
David Leoni <noreply@davidleoni.it>
2020-02-18 14:28:17 +0100

    fixed missing google verification token

--- conf.py
+++ conf.py
@@ -231,9 +231,9 @@ templates_path = ['_templates']
 
 # NOTE: don't put the extension on the left  !
 
-#html_additional_pages = {
-#    'google3dea3b29336ca0e5': 'google3dea3b29336ca0e5.html',
-#}
+html_additional_pages = {
+    'google3dea3b29336ca0e5': 'google3dea3b29336ca0e5.html',
+}
 
 
 
