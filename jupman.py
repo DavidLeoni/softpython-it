a6ca90e6c66c2b34bf64b1957267057d63dc295c
David Leoni <noreply@davidleoni.it>
2020-03-20 09:37:16 +0100

    Fix for cross visualizer arrow jumps in Python Tutor
    Merged from https://github.com/DavidLeoni/jupman/issues/25

--- jupman.py
+++ jupman.py
@@ -9,21 +9,21 @@ import unittest
 import inspect
 import os
 import argparse
-        
 
 def detect_relpath(in_cells):
     """ Hacky way to find out relative path to jupman.py
+
         in_cells: "In" cells of a notebook
     """
     import re        
-
-
+        
     for code in in_cells:        
         rs =  re.findall(r'import\s+sys\s*;?\s*\nsys\.path.append\([\'\"]((\.\./)+)[\'\"]\)\s*;?\s*\nimport\s+jupman', code)
-
+        
         if rs:
             return rs[0][0]   
     return ''
+
 
 def init(toc=False):
     """ Injects notebooks with js and css from _static
@@ -2596,9 +2596,11 @@ def pytut():
     """ Embeds a Python Tutor in the output of the current Jupyter cell,
         Code to execute is taken from *current* cell stripped from 
         the call to pytut() itself. 
+
         - The GUI will also be shown on the built Sphinx website.
         - Does *not* requires internet connection
         - ... and yes, implementation is very hacky
+
         Author: David Leoni <info@davidleoni.it>
     """
     #Hacky way to get variables from stack, but if we use %run -i we don't need it.
@@ -2632,6 +2634,7 @@ def pytut():
                           right before its call. 
                           
         For example: 
+
             x = 5
             y = x + 3
             jupman.pytut()
@@ -2650,48 +2653,55 @@ def pytut():
     relpath = detect_relpath(notebook_globals["In"]) 
     
     inject = ""
-
+    
+    # will end up reloading multiple times the script, not very efficient 
     inject +=  """
-            <script src="%s_static/js/pytutor-embed.bundle.min.js" type="application/javascript"></script>
+        <script src="%s_static/js/pytutor-embed.bundle.min.js" type="application/javascript"></script>
     """ % relpath
                     
     inject += """ 
-                    <script id="%s" type="application/json" >
-                        %s
-                    </script>
-                    <div id="%s" class="pytutorVisualizer"> </div>
-            """ % (json_id, trace, div_id)
+        <script id="%s" type="application/json" >
+            %s
+        </script>
+        <div id="%s" class="pytutorVisualizer"> </div>
+""" % (json_id, trace, div_id)
     inject += """ 
-                    <style>
-                    .vizLayoutTd {
-                        background-color: #fff !important;
-                    }
-                                        
-                    /* 'Edit this code' link, hiding because replaces browser tab !!!*/
-                    #editCodeLinkDiv {
-                        display:none;  
-                    }
-                    </style>   
+        <style>
+        .vizLayoutTd {
+            background-color: #fff !important;
+        }
+                            
+
+        /* 'Edit this code' link, hiding because replaces browser tab !!!*/
+        #editCodeLinkDiv {
+            display:none;  
+        }
+        </style>   
     """
     inject +=   """                        
-                <script>
-                (function(){
-                    var trace = JSON.parse(document.getElementById('%s').innerHTML);
-                    // NOTE: id without #
-                    addVisualizerToPage(trace, '%s',{'embeddedMode' : false})  
-                    
-                    
-                    // set overflow for pytuts - need to do in python as css 
-                    // does not allow parent selection
-                    var pytuts = $('.pytutorVisualizer')
-                    pytuts.closest('div.output_html.rendered_html.output_result')
-                            .css('overflow-x', 'visible')
-                
-                    //pytuts.closest('div.output_html.rendered_html.output_result')
-                    //      .css('background-color','red')                
-                    
-                })()
-                </script>
+        <script>
+        (function(){
+
+            var trace = JSON.parse(document.getElementById('%s').innerHTML);                                        
+            // NOTE 1: id without #
+            // NOTE 2 - maybe there are more predictable ways, but this will work anyway
+            //        - id should be number
+            visualizerIdOverride = Math.trunc(Math.random() * 100000000000)
+            addVisualizerToPage(trace, '%s',{'embeddedMode' : false,
+                                             'visualizerIdOverride':visualizerIdOverride})  
+            
+            
+            // set overflow for pytuts - need to do in python as css 
+            // does not allow parent selection
+            var pytuts = $('.pytutorVisualizer')
+            pytuts.closest('div.output_html.rendered_html.output_result')
+                    .css('overflow-x', 'visible')
+        
+            //pytuts.closest('div.output_html.rendered_html.output_result')
+            //      .css('background-color','red')                
+            
+        })()
+        </script>
                 
                 """ % (json_id, div_id)   
     
